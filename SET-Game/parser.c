273f175bd272354677aa08cbe7a01c148bb4d4b2
Ben Heng <benheng@bu.edu>
2013-12-09 22:46:28 -0500

    Added capacity for commands

--- SET-Game/parser.c
+++ SET-Game/parser.c
@@ -8,12 +8,12 @@
 #define MAX_IN 9		// max input "xx xx xx\0" = 8 chars
 #define DELIMS " \t\r\n"
 
-int cards[3];
+int cards[4];
 
 int* get_user_input () {
 
 	while (1) { // continue to get input until a valid input is parsed
-		printf ("Select 3 cards delimited by spaces. ex: 10 12 7\n#: ");
+		printf ("#: ");
 		fflush (stdout);
 		
 		// reset user input array
@@ -26,44 +26,60 @@ int* get_user_input () {
 		}
 		
 		// reset array
-		cards[0] = 0; cards[1] = 0; cards[2] = 0;
+		cards[0] = 0; cards[1] = 0; cards[2] = 0, cards[3] = 3;
 		char *token = {0};
 		int i = 0;
 		
 		// parse input into seperate tokens and store them into an array
 		if (input != NULL && (input[0] != '\n')) {
 			token = strtok (input, DELIMS);		// retrieve first token
-			
-			////////////////////////////////////////////////////////////////////
-			while (token != NULL && i < 3) {	// parse the token
-				errno = 0;
-				char *garbage = NULL;
-				long l_value = strtol(token, &garbage, 0);
-				switch (errno) {
-					case ERANGE:
-						printf("ERROR: Input cannot be represented as an integer.\n"
-						       "       Out of (long int) range. Please try again.\n");
-						break;
-					case EINVAL:
-					    printf("ERROR: Unsupported base / radix. Please try again.\n");
-					    break;
-				}
-				if (strlen(garbage) > 0) {
-					printf("ERROR: Input contains unsupported characters for base: %s. "
-					       "Please try again.\n", garbage);
-					errno = 98;
-				}
-				if (!(l_value >= 0 && l_value < MAX_CARDS)) {
-					printf("ERROR: Input out of range of selectable cards.\n");
-					errno = 99;
-				}
-				if (errno != 0) break;
+			for (;*token;++token) *token = tolower(*token);
 
-				// only valid inputs make it pase this point
-				cards[i++] = l_value;
-				token = strtok (NULL, DELIMS); // retrieve the next token
+			// OPTIONS //////////////////////////////////////////////////////////
+			if (strcmp(token, "exit") || strcmp(token, "quit")) {
+				cards[3] = 0;
+				return cards;
 			}
-			////////////////////////////////////////////////////////////////////
+			else if (strcmp(token, "cards")) {
+				cards[3] = 1;
+				return cards;
+			}
+			else if (strcmp(token, "results")) {
+				cards[3] = 2;
+				return cards;
+			}
+			else {
+				////////////////////////////////////////////////////////////////////
+				while (token != NULL && i < 3) {	// parse the token
+					errno = 0;
+					char *garbage = NULL;
+					long l_value = strtol(token, &garbage, 0);
+					switch (errno) {
+						case ERANGE:
+							printf("ERROR: Input cannot be represented as an integer.\n"
+							       "       Out of (long int) range. Please try again.\n");
+							break;
+						case EINVAL:
+						    printf("ERROR: Unsupported base / radix. Please try again.\n");
+						    break;
+					}
+					if (strlen(garbage) > 0) {
+						printf("ERROR: Input contains unsupported characters for base: %s. "
+						       "Please try again.\n", garbage);
+						errno = 98;
+					}
+					if (!(l_value >= 0 && l_value < MAX_CARDS)) {
+						printf("ERROR: Input out of range of selectable cards.\n");
+						errno = 99;
+					}
+					if (errno != 0) break;
+	
+					// only valid inputs make it pase this point
+					cards[i++] = l_value;
+					token = strtok (NULL, DELIMS); // retrieve the next token
+				}
+				////////////////////////////////////////////////////////////////////
+			}
 		}
 		if (errno != 0) continue;
 		if (i > 2) break;
